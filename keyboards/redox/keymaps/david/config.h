6d8c1fd177bd055e4689fffa2b35e1baad4b45b5
David <david.m.richter@web.de>
2020-10-03 13:20:17 +0200

    Umlaute

--- keyboards/redox/keymaps/david/config.h
+++ keyboards/redox/keymaps/david/config.h
@@ -39,3 +39,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Allows media codes to properly register in macros and rotary encoder code
 #define TAP_CODE_DELAY 10
+
+#define TAPPING_TERM 200
+//#define IGNORE_MOD_TAP_INTERRUPT
+//#undef PERMISSIVE_HOLD
+//#undef PREVENT_STUCK_MODIFIERS
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
