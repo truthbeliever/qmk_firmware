018c664547c340911b4eb3607b5ffb8bac64b2a6
David <david.m.richter@web.de>
2020-10-03 13:44:16 +0200

    Optimized Mouse

--- keyboards/redox/keymaps/david/rules.mk
+++ keyboards/redox/keymaps/david/rules.mk
@@ -1,3 +1,2 @@
 UNICODE_ENABLE = yes       # Unicode
-//LEADER_ENABLE = yes        # Enable the Leader Key feature
-//TAP_DANCE_ENABLE = yes
+LEADER_ENABLE = yes        # Enable the Leader Key feature
