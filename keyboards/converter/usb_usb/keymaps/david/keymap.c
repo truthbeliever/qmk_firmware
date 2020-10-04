5973a51f0aca0ba33225fbbcdc6d771eea99c56b
David <david.m.richter@web.de>
2020-10-04 19:14:33 +0200

    Hasu Space shift

--- keyboards/converter/usb_usb/keymaps/david/keymap.c
+++ keyboards/converter/usb_usb/keymaps/david/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,          KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
     KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,   KC_M,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_NUHS, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_NUBS,KC_K,   KC_H,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RO,   KC_RSFT,              KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
-    KC_LCTL, KC_LALT, MO(_NAV), XXXXXXX, XXXXXXX,         KC_SPC ,        XXXXXXX, XXXXXXX, XXXXXXX,  MO(_NAV),  KC_RALT, XXXXXXX, KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
+    KC_LCTL, KC_LALT, MO(_NAV), XXXXXXX, XXXXXXX,        MT(MOD_RSFT,KC_SPC),        XXXXXXX, XXXXXXX, XXXXXXX,  MO(_NAV),  KC_RALT, XXXXXXX, KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
     ),
     [_NAV] = LAYOUT_all(
                       ______,  ______,  ______, ______, ______,  ______,   ______, ______,  ______, ______, ______, ______,
